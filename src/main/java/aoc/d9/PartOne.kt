08314f27e9ddd719301329ee6a4aa10d73bbc972
Rahul.Jain <rahulj51@gmail.com>
2021-12-11 20:25:10 +0100

    Refactored

--- src/main/java/aoc/d9/PartOne.kt
+++ src/main/java/aoc/d9/PartOne.kt
@@ -13,36 +13,46 @@ fun readTestFile(): List<String> {
     return readFile("src/main/java/aoc/d9/input_test.dat")
 }
 
-typealias Point = Pair<Int,Int>
+data class Point(
+    val x : Int,
+    val y : Int
+) {
+
+    fun adjacent(): List<Point> =
+        listOf(
+            Point(this.x+1, this.y),
+            Point(this.x-1, this.y),
+            Point(this.x, this.y+1),
+            Point(this.x, this.y-1)
+        )
+
+}
 
 fun Array<IntArray>.get(p:Point): Int =
-    this[p.second][p.first]
+    this[p.y][p.x]  //each row is y, each col is x
 
 fun Array<IntArray>.contains(p:Point): Boolean =
-    p.second in this.indices && p.first in this[p.second].indices
+    p.y in this.indices &&
+            p.x in this[p.y].indices
 
-fun Point.adjacent(): List<Point> =
-    listOf(
-        Point(this.first+1, this.second),
-        Point(this.first-1, this.second),
-        Point(this.first, this.second+1),
-        Point(this.first, this.second-1)
-    )
 
 fun main() {
 
-    val readings = readInputFile().map { it -> it.map { d -> d.digitToInt() }.toIntArray() }.toTypedArray()
+    val readings = readInputFile()
+        .map { it ->
+            it.map { d -> d.digitToInt() }.toIntArray() } //convert each row to an int array
+        .toTypedArray() //add to a second array
 
-    val lowestPoints = readings.mapIndexed { yIndex, row ->
+    val lowPoints = readings.mapIndexed { yIndex, row ->
         row.mapIndexed { xIndex, col ->
             val p = Point(xIndex, yIndex)
             val value = col
             val neighborValues = p.adjacent().filter { readings.contains(it) }
-                .map { readings[it.second][it.first] }
+                .map { readings[it.y][it.x] }
             if (value < neighborValues.minOf { it }) p else null
         }
     }.flatten().filterNotNull()
 
-    println(lowestPoints.map { readings.get(it)+1 }.sum())
+    println(lowPoints.map { readings.get(it)+1 }.sum())
 
 }
